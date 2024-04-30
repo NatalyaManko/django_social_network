c330934ec5792bcda974b51ce5fca5474104acc2
Наталья Манько <138647515+NatalyaManko@users.noreply.github.com>
2024-04-30 11:49:28 +0700

    Update settings.py

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dkuxb%tq$n52hpj7&2e+h0e5(+pi7*5f*w+j9@=&%$(n-466k#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
